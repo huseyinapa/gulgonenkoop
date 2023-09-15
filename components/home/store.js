3986e01dd60925a84462c1d9fb846661c308146a
Huseyin <huso.apa32@gmail.com>
2023-09-16 01:20:41 +0300

    15.09

--- components/home/store.js
+++ components/home/store.js
@@ -1,44 +1,50 @@
 import React from "react";
-import Image from "next/image";
 
 const Store = () => {
   return (
     <div className="bg-gradient-to-r from-blue-500 to-purple-500 text-white min-h-screen flex flex-col relative">
-      <div className="absolute top-0 left-0 z-10 p-6">
-        <h1
-          className="text-2xl font-semibold"
-          style={{
-            fontFamily: "The Mumbai Sticker",
-            color: "white",
-            textShadow: "0px 0px 10px #FF00D6",
-          }}
-        >
-          GülGönen
-        </h1>
-      </div>
-      <header className="container mx-auto px-6 text-center py-12 relative z-10">
-        <h1 className="text-5xl font-bold mb-6 animate__animated animate__fadeIn animate__delay-1s">
-          Kooperatifimize Hoş Geldiniz!
-        </h1>
-        <p className="text-lg mb-8 animate__animated animate__fadeIn animate__delay-2s">
-          Kooperatifimizde birlikte çalışarak en iyi sonuçları elde edebiliriz.
-          Size özel fırsatları kaçırmayın!
-        </p>
-        <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 justify-center animate__animated animate__fadeIn animate__delay-3s">
-          <a
-            href="#"
-            className="px-6 py-3 bg-blue-500 text-white hover:bg-blue-600 rounded-lg transition-colors duration-300"
-          >
-            Hemen Katılın
-          </a>
-          <a
-            href="#"
-            className="px-6 py-3 bg-white text-blue-500 hover:bg-indigo-600 border border-blue-500 rounded-lg transition-colors duration-300"
-          >
-            Bizimle İletişime Geçin
-          </a>
+      <div className="absolute top-1/2 left-10 z-10 p-6 text-white flex flex-col space-y-4 transform -translate-y-1/2">
+        <div className="max-w-lg">
+          <h2 className="text-4xl font-bold text-center">
+            Kooperatifimizin Hikayesi
+          </h2>
+          <p className="text-lg">
+            Kooperatifimiz 20 yıldan fazla bir süredir faaliyet göstermektedir.
+            Amacımız çiftçilere destek sağlamak ve sürdürülebilir tarımı teşvik
+            etmektir. Size özel fırsatlarımızı kaçırmayın!
+          </p>
+          <div className="space-y-4">
+            <a
+              href="#"
+              className="px-6 py-3 bg-blue-500 text-white hover:bg-blue-600 rounded-lg transition-colors duration-300"
+            >
+              Daha Fazla Bilgi
+            </a>
+            <a
+              href="#"
+              className="px-6 py-3 bg-white text-blue-500 hover:bg-indigo-600 border border-blue-500 rounded-lg transition-colors duration-300"
+            >
+              Bizimle İletişime Geçin
+            </a>
+          </div>
         </div>
-      </header>
+      </div>
+      <div className="absolute top-10 right-20 z-10 p-6 text-white flex flex-col items-center space-y-4">
+        <img
+          src="/images/ggkoop.png"
+          alt="GG Koop"
+          className="w-[450px] h-auto my-4 rounded-full object-contain"
+          width={"96"}
+          height={"96"}
+        />
+        <img
+          src="/images/ggkoop2.png"
+          alt="GG Koop2"
+          className="w-[450px] h-auto my-4 rounded-full object-contain"
+          width={"80"}
+          height={"80"}
+        />
+      </div>
       <div className="absolute inset-0 z-0">
         <div
           className="h-full w-full bg-cover bg-center filter blur-lg"
