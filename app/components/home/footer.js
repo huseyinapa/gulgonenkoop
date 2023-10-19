3565e8c031c3e3d1626c5ce44dc8d7f9fd10adc1
Huseyin <huso.apa32@gmail.com>
2023-10-20 00:28:05 +0300

    19.10

--- app/components/home/footer.js
+++ app/components/home/footer.js
@@ -38,7 +38,7 @@ export default function Footer() {
           </Link>
           <div className="flex flex-col items-start ml-5 space-y-1">
             <h1>
-              <span className="font-semibold text-base md:text-md">Apa Dijital Ajans</span>
+              <span className="font-semibold text-base md:text-md">APA Dijital Ajans</span>
             </h1>
             <h1>
               <span className="text-md">tarafından geliştirilmiştir.</span>
@@ -95,7 +95,7 @@ export default function Footer() {
               <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
             </svg>
           </a>
-          <a>
+          <a href="https://www.facebook.com/groups/388470874661959/" target="_blank">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="24"
