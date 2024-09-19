ef0ab6900d10bc389f42e3c725c422cee0b2c0da
Huseyin <huso.apa32@gmail.com>
2024-09-19 03:22:00 +0300

    ürün detayları 80% sadece responsive kaldı

--- app/products/[productId]/page.js
+++ app/products/[productId]/page.js
@@ -1,10 +1,14 @@
-import Footer from "@/app/components/home/footer";
-import Header from "@/app/components/home/header";
-import axios from "axios";
-import { ArrowLeft } from "lucide-react";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import axios from "axios";
+import { ArrowLeft } from "lucide-react";
+
+import Footer from "@/app/components/home/footer";
+import Header from "@/app/components/home/header";
+
+import AddToCartButton from "@/components/products/addToCartComp";
+import ProductList from "@/components/products/productList";
 
 async function getProductData(productId) {
   try {
@@ -41,7 +45,6 @@ function extractIdFromUrl(url) {
 
 export default async function ProductPage({ params: { productId } }) {
   console.log(productId);
-  console.log(productId.split('-'));
 
   const id = extractIdFromUrl(productId);
 
@@ -52,44 +55,61 @@ export default async function ProductPage({ params: { productId } }) {
 
   return (
     <div className="min-w-fit">
+
       <Header />
+
       <Head>
-        <title>{product.name} - Gülgönen Koop</title>
-        <meta name="description" content={product.description} />
+        <title>Ürün Detayı - Gülgönen Koop.</title>
+        <meta name="description" content={`Gülgönen Koop. üzerinden ${product?.name} ürününü inceleyin.`} />
       </Head>
-      <div className="container flex flex-row items-center justify-between p-6 mx-auto bg-slate-300">
+
+      <div className="container flex flex-row items-center justify-between p-6 mx-auto">
         <Link
           href="/products"
-          className="duration-200 text-rose-600 hover:text-rose-400"
+          className="flex items-center gap-4 duration-200 text-secondary hover:text-[#cc5c8b]"
         >
           <ArrowLeft className="w-6 h-6" />
+          <h1 className="font-bold text-2xl text-pretty">{product.name} - {product.size} {product.type}</h1>
         </Link>
       </div>
 
-      <div className="container flex flex-row items-center p-6 mx-auto bg-slate-400 sticky">
-        <div key={product.id} className="flex flex-row space-x-6">
-          <Image
-            src={product.webpath}
-            className="size-60 bg-slate-200 rounded-lg"
-            width={20}
-            height={20}
-          />
-          <div className="container">
-            <h1 className="font-bold text-2xl text-secondary text-pretty mb-2">{product.name}</h1>
+      <div className="container flex flex-row items-start p-6 mx-auto">
+        <div key={product.id} className="flex flex-row space-x-4 w-[65%]">
+          <figure className="relative">
+            <Image
+              src={product.webpath}
+              className="w-96 py-4 object-contain rounded-lg"
+              width={20}
+              height={20}
+            />
+            <div className="absolute top-0 left-0 flex flex-col bg-green-800 size-[70px] rounded-full text-white items-center justify-center font-bold">
+              <span>
+                %100
+              </span>
+              <span>
+                doğal
+              </span>
+            </div>
+          </figure>
+          <div className="container flex flex-col gap-2 justify-between">
+            <div className="flex flex-row items-center">
+              <h1 className="font-bold text-2xl text-secondary text-pretty mr-auto">{product.name}</h1>
+            </div>
             {/* Ürün açıklamaları */}
-            <div className="flex flex-col">
+            <div className="flex flex-col gap-2">
               <p className="font-medium text-md text-secondary">{product.description}</p>
-              <p>{product.price.toLocaleString("tr-TR", {
-                style: "currency",
-                currency: "TRY",
-              })}</p>
               <p className={product.stock > 0 ? "text-green-800" : "text-red-500"}>
                 {product.stock > 0 ? "Stokta var" : "Stokta yok"}
               </p>
               <p>{product.size} {product.type}</p>
-
+            </div>
+            <div>
+              <AddToCartButton product={product} />
             </div>
           </div>
+        </div>
+        <div className="flex flex-col space-x-4 w-[35%]">
+          <ProductList excludingProductId={product.id} />
         </div>
       </div>
 
---
+++ components/products/productList.js
@@ -0,0 +1,188 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import Link from "next/link";
+
+import CartManager from "@/app/utils/cart";
+import ProductManager from "@/app/utils/product";
+
+export default function ProductList({ excludingProductId }) {
+    const [isAdmin, setIsAdmin] = useState(false);
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    const productManager = new ProductManager();
+    const cartManager = new CartManager();
+
+    useEffect(() => {
+        checkIsAdmin();
+        getProducts();
+    }, []);
+
+    function checkIsAdmin() {
+        const getPermission = parseInt(localStorage.getItem("permission")) ?? 0;
+        setIsAdmin(getPermission === 1);
+    }
+
+    async function getProducts() {
+        try {
+            const products = await productManager.fetchProducts();
+            if (products !== null) {
+                setProducts(products.filter((e) => e.id !== excludingProductId));
+            } else {
+                setProducts(null);
+                toast.error("Stoğumuzda ürün bulunmuyor.");
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error("Ürünler getirilirken bir hata oluştu.");
+        }
+    }
+
+
+    return (
+        <div className="">
+            {loading && (
+                <div className="flex justify-center items-center">
+                    <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
+                </div>
+            )}
+            <div
+                className={`flex flex-wrap mx-auto justify-center sm:items-center grid-cols-2 md:grid-cols-3 gap-4 md:gap-6
+          ${loading ? "opacity-50" : ""}
+          `}
+            >
+                {products.length === 0 ? (
+                    <div></div>
+                ) : (
+                    products.map((product) => (
+                        <ProductCard key={product.id} product={product} />
+                    ))
+                )}
+            </div>
+        </div>
+    );
+
+    function slugify(text) {
+        const turkishMap = {
+            'ç': 'c', 'Ç': 'C',
+            'ğ': 'g', 'Ğ': 'G',
+            'ı': 'i', 'İ': 'I',
+            'ö': 'o', 'Ö': 'O',
+            'ş': 's', 'Ş': 'S',
+            'ü': 'u', 'Ü': 'U'
+        };
+
+        return text
+            .toLowerCase()
+            .replace(/[çÇğĞıİöÖşŞüÜ]/g, function (match) {
+                return turkishMap[match];
+            })
+            .replace(/[^a-z0-9\-]/g, '-')  // Alfanümerik olmayan karakterleri - ile değiştir
+            .replace(/-+/g, '-')           // Birden fazla "-" varsa tek "-"e indir
+            .replace(/^-|-$/g, '');
+    }
+
+    function ProductCard({ product }) {
+        const [image, setImage] = useState(product.image);
+
+        return (
+            <div
+                key={product.id}
+                className="card bg-white text-neutral-content w-[170px] sm:w-[180px] md:w-[260px] h-[330px] md:h-[400px]  shadow-[#FFA4D5] shadow-[0_0_40px_3px]"
+            >
+                <figure className="relative">
+                    <Link href={`/products/${slugify(product.name)}-${product.id.toLowerCase()}`}>
+                        <img
+                            src={image || product.webpath}
+                            alt={product.name}
+                            className="h-[160px] sm:h-[180px] md:h-[200px] w-72 object-cover rounded-t-lg"
+                            onError={() => {
+                                setImage(product.webpath);
+                            }}
+                            onClick={() => {
+
+                            }}
+                        />
+                    </Link>
+
+                    <div className="absolute bg-secondary w-14 sm:w-20 h-6 sm:h-8 md:h-10 place-content-center bottom-3 right-0 rounded-l-xl">
+                        <span className="pl-4 text-xs sm:text-sm md:text-md  font-bold">
+                            {product.size} {product.type}
+                        </span>
+                    </div>
+                </figure>
+                <div className="card-body relative mx-auto p-0 pt-3 px-1">
+                    <h1 className="card-title text-base md:text-lg justify-center text-[#8a4269] font-bold">
+                        {product.name}
+                    </h1>
+                    <div className="divider bg-[#e2a9c8] h-[1px] w-36 md:w-48 m-auto"></div>
+                    <p className="text-base text-center md:text-lg text-[#8a4269]">
+                        {product.description}
+                    </p>
+                </div>
+                <div className="card-actions flex-col-reverse sm:flex-row sm:justify-between items-center gap-3 pb-3 sm:p-3">
+                    <button
+                        className="btn btn-xs md:btn-sm w-32 h-8 bg-secondary text-white"
+                        onClick={() => handleAddCart(product)}
+                    >
+                        Sepete Ekle
+                    </button>
+                    <div className="text-[#8a4269] font-semibold text-base">
+                        {parseInt(product.price).toLocaleString("tr-TR", {
+                            style: "currency",
+                            currency: "TRY",
+                        })}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    async function handleAddCart(data) {
+        var id = localStorage.getItem("id") ?? null;
+        if (id === null) {
+            toast.error(
+                "Sepete ürün eklemek için kayıt olmanız/giriş yapmanız gerekir."
+            );
+            return;
+        }
+
+        try {
+            const productData = await productManager.getProduct(data.id);
+
+            const productInCartForm = new FormData();
+            productInCartForm.append("id", id);
+            productInCartForm.append("pid", data.id);
+
+            const cartProductData = await cartManager.getProductInCart(
+                productInCartForm
+            );
+
+            if (productData !== null && productData.stock < 1) {
+                return toast.error("Ürün stokta bulunmuyor.");
+            } else if (
+                cartProductData !== null &&
+                cartProductData.amount >= productData.stock
+            ) {
+                return toast.error(`Stoktaki tutardan fazlası sepete eklenemez.`);
+            }
+
+            const formData = new FormData();
+            formData.append("id", id); // Müşteri kimliği
+            formData.append("pid", data.id);
+            formData.append("amount", "1");
+            formData.append("date", Date.now().toString());
+
+            await toast.promise(cartManager.add(formData), {
+                loading: "Ekleniyor...",
+                success: "Ürün sepete eklendi!",
+                error: "Ürün sepete eklenemedi.",
+            });
+        } catch (error) {
+            toast.error("Bilinmeyen hata. Kod: PC-HAC");
+            console.log(error);
+        }
+    }
+}
