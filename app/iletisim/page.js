9082405cb6267a766d3df7641576fc5257fde74a
Hüseyin APA <81553048+huseyinapa@users.noreply.github.com>
2024-12-06 01:52:47 +0300

    İletişim sayfasındaki gereksiz yorum satırları kaldırıldı ve telefon bağlantısı güncellendi.

--- app/iletisim/page.js
+++ app/iletisim/page.js
@@ -9,9 +9,6 @@ const Contact = () => {
       <Header />
       <h1 className="mt-3 text-center font-semibold text-2xl">İletişim</h1>
 
-
-
-      {/* İçerik */}
       <div className="relative px-4 mx-auto lg:max-w-5xl mt-6 mb-6 space-y-2">
         <div>
           <h2 className="font-semibold mb-2">Kooperatif Bilgileri:</h2>
@@ -39,24 +36,12 @@ const Contact = () => {
             <li>Zübeyde Kutlu:</li>
             <li>
               Telefon:{" "}
-              <a href="tel:+905" className="text-[#b64983]">Eklenecek</a>
+              <a href="" className="text-[#b64983]">Eklenecek</a>
             </li>
           </div>
         </div>
       </div>
-      {/* Arka Plan */}
-      <div className="relative">
-        <div className="absolute inset-0 z-10">
-          <div className="grid grid-cols-2 -space-x-52">
-            {/* Arka plan renkleri daha belirgin hale getirildi */}
-            <div className="blur-[106px] h-72 bg-gradient-to-br from-[#d82685] to-[#d82685]/50"></div>
-            <div className="blur-[106px] h-48 bg-gradient-to-r from-[#d82685] to-[#d82685]/20"></div>
-          </div>
-        </div>
-        <div className="px-4 lg:max-w-5xl mx-auto my-3 text-left space-y-5">
-          <p className="my-4">Son Güncelleme Tarihi: 4 Aralık 2024</p>
-        </div>
-      </div>
+
       <Footer />
     </main>
   );
